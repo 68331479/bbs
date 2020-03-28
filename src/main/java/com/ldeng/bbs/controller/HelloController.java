2130edabcc87a95e50c9769b4f1dba9fa07d5622
Lei Deng <68331479@QQ.COM>
2020-03-28 13:16:47 +0800

    20200328

---
+++ src/main/java/com/ldeng/bbs/controller/HelloController.java
@@ -0,0 +1,22 @@
+package com.ldeng.bbs.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * @Author: LDeng
+ * @Date: 2020-03-28 12:38
+ */
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(name="name")String name, Model model){
+
+        model.addAttribute("name",name);
+        return "hello";
+    }
+
+}
